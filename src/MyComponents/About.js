b291a3ced10e5657deaf8f5ae92222297b8b880b
Arihant Jain <arihantj916@gmail.com>
2023-08-02 16:56:44 +0530

    Todo App

---
+++ src/MyComponents/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../index.css";
+
+export const About = () => {
+  return (
+    <div className="about">
+        <h2>
+      This is About Page
+        </h2>
+      <p >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus itaque
+        veniam omnis nostrum odio eligendi, quaerat corporis? Ratione, in fugit
+        placeat sint tempore est dolorem ipsum ut ipsam facilis veniam velit
+        saepe expedita fugiat. Modi ullam, inventore iure officia quas magnam
+        natus eos explicabo temporibus, eum consequuntur voluptate voluptatem
+        corrupti!
+      </p>
+    </div>
+  );
+};
